f1e3d3de6bc41f05b1df4483e83c1371dc8b23b6
feyton <tumbafabruce@gmail.com>
2020-08-03 16:49:47 +0200

    styling a permit

--- permit/utils.py
+++ permit/utils.py
@@ -5,22 +5,26 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 from django.utils import timezone
 from xhtml2pdf import pisa
+import random
 
 
 def permit_code():
-    return datetime.now()
+    string = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+    randomstr = ''.join((random.choice(string)) for x in range(4))
+    return '%s-%s' % (datetime.now().strftime('%Y%m%d'), randomstr)
+
 
 def start_date_default():
     return timezone.now() + timezone.timedelta(days=1)
+
 
 def end_date_default():
     return timezone.now() + timezone.timedelta(days=2)
 
 
-
 def render_to_pdf(template_src, context_dict={}):
     template = get_template(template_src)
-    html  = template.render(context_dict)
+    html = template.render(context_dict)
     result = BytesIO()
     pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
     if not pdf.err:
